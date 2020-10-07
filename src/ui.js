158afd200b80201848ee3230e2aa18374e9204f2
tycrek <josh.moore@jmoore.dev>
2020-10-07 15:02:29 -0600

    get and display application assets

--- src/ui.js
+++ src/ui.js
@@ -1,26 +1,31 @@
 // Imports
+const { RPC_DATA } = require('./utils');
 const { app, BrowserWindow } = require('electron');
 const Pug = require('pug');
 const fs = require('fs-extra');
 const path = require('path');
+const RPC = require('./rpc');
 
 // Constants
 const htmlFile = path.join(__dirname, 'frontend/index.html');
-const pugFile = Pug.renderFile(path.join(__dirname, 'frontend/index.pug'));
+const pugFile = path.join(__dirname, 'frontend/index.pug');
 const windowData = {
 	width: 360,
-	height: 240,
+	height: 300,
 	alwaysOnTop: true,
 	fullscreenable: false,
 	opacity: 0.8,
-	resizable: false
+	resizable: true
 };
 
 // Exports
 module.exports = {
 	open: () =>
 		new Promise((resolve, reject) =>
-			fs.writeFile(htmlFile, pugFile)
+			RPC.getAssetList(RPC_DATA.clientId)
+				.then((assetList) => assetList.map((asset) => RPC.getAssetImage(RPC_DATA.clientId, asset.id)))
+				.then((assets) => Pug.renderFile(pugFile, { assets }))
+				.then((pug) => fs.writeFile(htmlFile, pug))
 				.then(() => app.whenReady())
 				.then(() => new BrowserWindow(windowData))
 				.then((win) => win.loadFile(htmlFile))
