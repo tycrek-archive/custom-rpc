2a2a91ab1eba940873b46a5da85c8479315de8f2
tycrek <josh.moore@jmoore.dev>
2020-10-07 00:09:40 -0600

    idfk

--- rpc.js
+++ rpc.js
@@ -1,11 +1,18 @@
-require('dotenv').config();
-
+// General imports
+const path = require('path');
+const fs = require('fs-extra');
+const express = require('express');
+const { app, BrowserWindow } = require('electron');
+const Pug = require('pug');
 const Discord = require('discord-rpc');
+
+// Constants
+const server = express();
 const client = new Discord.Client({ transport: 'ipc' });
 
 const EMPTY_STATE = '‎‎'; //* This string actually has a length of two. The characters are from: https://emptycharacter.com/
 const RPC_DATA = {
-	clientId: process.env.CLIENT_ID,
+	clientId: '763074590149836821',
 	defaultData: {
 		details: 'Deciding what to do',
 		state: EMPTY_STATE,
@@ -14,14 +21,52 @@ const RPC_DATA = {
 	}
 };
 
+//! Express
+server.get('/setrpc/:topline/:bottomline', (req, res) => {
+	let details = Buffer.from(req.params.topline, 'base64').toString();
+	let state = Buffer.from(req.params.bottomline, 'base64').toString();
+	setActivity({
+		details,
+		state: state === '~~USE_EMPTY~~' ? EMPTY_STATE : state,
+		startTimestamp: Date.now(),
+		largeImageKey: 'tycrek'
+	})
+		.then(() => console.log(':: [DISCORD] :: Presence updated'))
+		.then(() => res.send('Success'))
+		.catch((err) => (console.warn(err), res.send('Error')))
+});
+server.listen(14374, () => console.log(':: [EXPRESS] :: Started backend'))
+
+//! Electron
+
+app.whenReady().then(() => {
+	console.log(':: [ELECTRON] :: Launching window...')
+	const win = new BrowserWindow({
+		width: 340,
+		height: 220,
+		alwaysOnTop: true,
+		fullscreenable: false,
+		kiosk: true,
+		opacity: 0.8,
+		resizable: false
+	});
+
+	fs.writeFile(path.join(__dirname, 'frontend/index.html'), Pug.renderFile(path.join(__dirname, 'frontend/index.pug')))
+		.then(() => win.loadFile(path.join(__dirname, 'frontend/index.html')))
+		.catch(console.warn);
+});
+
+//! Discord
+
+
 function _main() {
-	console.log(':: Connecting...');
+	console.log(':: [DISCORD] :: Connecting...');
 	connect()
-		.then(() => console.log(':: Connected'));
+		.then(() => console.log(':: [DISCORD] :: Connected'));
 
 	client.on('ready', () =>
 		setActivity(RPC_DATA.defaultData)
-			.then(() => console.log(':: Activity set'))
+			.then(() => console.log(':: [DISCORD] :: Activity set'))
 			.catch(console.warn));
 }
 
